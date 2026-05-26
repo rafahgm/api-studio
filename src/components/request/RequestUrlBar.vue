6b8aa19e02e95aff67b68f9b4acf4ca3fd522fb0
Rafael Morais <rafahgmorais@gmail.com>
2026-05-25 23:21:29 -0300

    refactor(ui): wrap request url bar in field group

--- src/components/request/RequestUrlBar.vue
+++ src/components/request/RequestUrlBar.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="flex items-center gap-2">
-    <USelect
-      v-model="request.method"
-      :items="methodOptions"
-      class="w-32"
-    />
+    <UFieldGroup class="w-full">
+      <USelect
+        v-model="request.method"
+        :items="methodOptions"
+        class="w-32"
+      />
 
-    <UInput
-      v-model="request.url"
-      placeholder="https://api.example.com/users"
-      class="flex-1"
-      @keydown.enter="requestStore.send"
-    />
+      <UInput
+        v-model="request.url"
+        placeholder="https://api.example.com/users"
+        class="flex-1"
+        @keydown.enter="requestStore.send"
+      />
+    </UFieldGroup>
 
     <UButton
       icon="i-lucide-send"
